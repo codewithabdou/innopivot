6768229ef2863eb4fac4c56f79144ffa0111cb37
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2024-11-22 22:58:33 +0100

    final fix

--- components/Charts/GanttChart.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { generateGantCropData } from "@/lib/generate-gantt-data";
-import Chart from "react-google-charts";
-
-const GanttChart = () => {
-  return <Chart chartType="Gantt" data={generateGantCropData()} />;
-};
-
-export default GanttChart;
